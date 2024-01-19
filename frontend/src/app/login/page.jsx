640c9d8a80a213188b5ebc50bbb3f515166dc9f8
CBStafford <c_b_stafford@yahoo.com>
2024-01-19 09:46:54 -0500

    fix login

---
+++ frontend/src/app/login/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+import LoginForm from '@/components/forms/login'
+
+
+export default function Login() {
+
+    return (
+        <div className="half-half page-content">
+            <h1>Login!#!#!#!#</h1>
+            <LoginForm />
+        </div>
+    )
+}
